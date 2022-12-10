85b539d0fe83dad53e0ab32c8032b0bbe6c8cc4d
Electra51 <safayetnurelectra@gmail.com>
2022-12-10 14:27:49 +0600

    initial setup and navbar

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+    daisyui: {
+    themes: [
+      {
+        pickColorTheme: {
+          primary: '#4a7887',
+          secondary: '#FEF9f6',
+          info:'#062029',
+          accent: '#F2E9E3',
+          neutral: '#000000', //text-black
+          'base-100': '#ffffff', //bg-white
+          'base-200': '#4a7887'
+        },
+      },
+      {
+        dark: {
+          info:'#062029',
+          secondary: '#1F2937', //black
+          accent: '#1F2937', 
+          neutral: '#ffffff', //text-white
+          'base-100': '#1F2937', //btn
+          'base-200': '#1F2937'
+        },
+      }
+    ]
+    
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        Satisfy: ['Satisfy', 'cursive'],
+        Inter:['Inter', 'sans-serif']
+      }
+    },
+  },
+  plugins: [require("daisyui")],
+}
